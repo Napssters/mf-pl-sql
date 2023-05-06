3a09deaf16983e71e9a7c884f4f617a4031722fb
Nicolas Orozco <nicolas.orozco@codesa.com.co>
2023-05-06 11:55:04 -0500

    creacion del proyecto

---
+++ src/app/feature/modulos-aprendizaje/modulos-aprendizaje.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModulosAprendizajeRoutingModule } from './modulos-aprendizaje-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ModulosAprendizajeRoutingModule
+  ]
+})
+export class ModulosAprendizajeModule { }

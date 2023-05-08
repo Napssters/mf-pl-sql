0b711463ad3ef962ff3631f20e4e57f3c69a1f75
Angello Gaitan Lopez <angello29@utp.edu.co>
2023-05-08 15:10:37 -0500

    estilos y primeng

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 
 
 @NgModule({
   declarations: [
-    FooterComponent
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule
